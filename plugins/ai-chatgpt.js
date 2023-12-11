32744f7dfa0c236b1eca518b8df38be39862cc60
DARK-LEGEND_HYPER <144076302+DarkLegendHyper@users.noreply.github.com>
2023-12-11 09:30:58 +0530

    Update ai-chatgpt.js

--- plugins/ai-chatgpt.js
+++ plugins/ai-chatgpt.js
@@ -2,7 +2,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { text,  usedPrefix,  command }) => {
-    if (!text) throw `Hey is there anything I can help you with??`
+    if (!text) throw `*hello user i am chat gpt and how can i help you..*`
     let order = { text: wait, mentions: [m.sender], contextInfo: { forwardingScore: 256, isForwarded: true }};
 let aii = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=${apilol}
 &text=${text}&user=user-unique-en`)
@@ -10,6 +10,7 @@ let aii = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=${apilol}
   let { key } = await conn.sendMessage(m.chat, order, { quoted: m });
   await new Promise(resolve => setTimeout(resolve, 2000));
   await conn.sendMessage(m.chat, { text: `${oke.result}`, edit: key }, { quoted: m });
+    m.react('🤖')
     }
 
 handler.help = ['ai', 'openai', 'gpt']
