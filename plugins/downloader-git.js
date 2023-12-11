f5e3bb8d7f01af57e8f79766a750d72a7de7e6bb
DARK-LEGEND_HYPER <144076302+DarkLegendHyper@users.noreply.github.com>
2023-12-11 10:00:29 +0530

    Update downloader-git.js

--- plugins/downloader-git.js
+++ plugins/downloader-git.js
@@ -2,12 +2,13 @@ import fetch from 'node-fetch'
 const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
 
 let handler = async (m, {conn, text, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Example user ${usedPrefix}${command} xxirfanx/zoromd`
+    if (!args[0]) throw `Example user ${usedPrefix}${command} github iteam name`
     let [usr, rep] = text.split`/`
     let url = `https://api.github.com/repos/${encodeURIComponent(usr)}/${encodeURIComponent(rep)}/zipball`
     let name = `${encodeURIComponent(rep)}.zip`
-    m.reply(`D o w n l o a d i n g. . .`)
+    m.reply(`*please waite uploadin your file*`)
     conn.sendFile(m.chat, url, name, null, m)
+    m.react('📩')
 }
 
 handler.help = ['gitclone <username>/<repo>']
