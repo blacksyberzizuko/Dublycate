cca66977d33dfa5b88bb42600655abb866a40950
DARK-LEGEND_HYPER <144076302+DarkLegendHyper@users.noreply.github.com>
2023-12-11 09:33:34 +0530

    Update cmd-list.js

--- plugins/cmd-list.js
+++ plugins/cmd-list.js
@@ -1,11 +1,12 @@
 let handler = async (m, { conn }) => {
     conn.reply(m.chat, `
-*LIST HASH*
+*QUEEN-SAKURA_LIST_CMD*
 \`\`\`
 ${Object.entries(global.db.data.sticker).map(([key, value], index) => `${index + 1}. ${value.locked ? `(Locked) ${key}` : key} : ${value.text}`).join('\n')}
 \`\`\`
 `.trim(), null, {
         mentions: Object.values(global.db.data.sticker).map(x => x.mentionedJid).reduce((a, b) => [...a, ...b], [])
+        m.react('📜')
     })
 }
 
