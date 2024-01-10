f79b2a21f60a7033c75b47a2eed92960201b3ac0
DARK-LEGEND_HYPER <144076302+DarkLegendHyper@users.noreply.github.com>
2024-01-10 10:21:07 +0530

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -8,7 +8,7 @@ if (process.send) { process.send('uptime')
 _muptime = await new Promise(resolve => { process.once('message', resolve) 
 setTimeout(resolve, 2000) }) * 1000}
 let uptime = clockString(_uptime)
-let bio = `${global.packname} 𝗧𝗛𝗘 𝗕𝗢𝗧 𝗥𝗨𝗡𝗧𝗜𝗠𝗘 𝗜𝗦 ${uptime} 𝗧𝗛𝗜𝗦 𝗜𝗦 𝗤𝗨𝗘𝗘𝗡-𝗦𝗔𝗞𝗨𝗥𝗔_𝗠𝗗 `
+let bio = `🧚‍♂️ 𝗤𝗨𝗘𝗘𝗡-𝗦𝗔𝗞𝗨𝗥𝗔_𝗩2🔱\n🧑‍💻𝗗𝗘𝗩𝗘𝗟𝗢𝗣𝗘𝗗 𝗕𝗬: 𝗠⦁𝗥 𝗗𝗔𝗥𝗞-𝗛𝗬𝗣𝗘𝗥\n🧚‍♂️𝗧𝗛𝗘 𝗕𝗢𝗧 𝗥𝗨𝗡𝗧𝗜𝗠𝗘 𝗜𝗦 ${uptime} `
 await this.updateProfileStatus(bio).catch(_ => _)
 setting.status = new Date() * 1
 } 
@@ -19,5 +19,5 @@ function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, ' » ', h, ' ・ ', m, ' ・ ', s].map(v => v.toString().padStart(2, 0)).join('') 
+  return [d, ' ⦁ ', h, ' ⦁ ', m, ' ⦁ ', s].map(v => v.toString().padStart(2, 0)).join('') 
 } 
