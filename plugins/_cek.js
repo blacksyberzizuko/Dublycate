0b6c33ce25d392a117dc478d24ab90f8f460277f
DARK-LEGEND_HYPER <144076302+DarkLegendHyper@users.noreply.github.com>
2024-01-11 10:19:36 +0530

    Update _cek.js

--- plugins/_cek.js
+++ plugins/_cek.js
@@ -1,6 +1,6 @@
 let handler = async ( m ) => {
-  m.reply('*👋 HELLO USER I AM ALIVE NOW..*')
-  m.react('👋')
+  m.reply('*👋 HELLO USER I AM ALIVE NOW*')
+  m.react("👋")
 }
 
 handler.command = /^(cek|tes|alive|p|a)$/i
