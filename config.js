3fbf35dff81f929658458132e5cdc084d6c3b06d
DARK-LEGEND_HYPER <144076302+DarkLegendHyper@users.noreply.github.com>
2024-01-15 12:21:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -51,8 +51,8 @@ global.l = '「'
 global.r = '」'
 global.wait = '```🕗please wait Processing Your request...```'
 global.eror = '```Error to main saver..```'
-globle.dl = '*📩 Please Wait Download Your Audio....*'
-globle.up = '*📤 Please Wait Uploading Your Audio....*'
+globle.wait2 = '*📩 Please Wait Download Your Audio....*'
+globle.wait3 = '*📤 Please Wait Uploading Your Audio....*'
 
 global.rpg = {
   emoticon(string) {
